0c7037715f9c7d0e3f8eadff3d788c9d0992f087
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2025-04-12 14:46:50 +0530

    Dcoker-compose add

--- CONFIG-SERVER/src/main/java/com/configServer/ConfigServerApplication.java
+++ CONFIG-SERVER/src/main/java/com/configServer/ConfigServerApplication.java
@@ -2,11 +2,10 @@ package com.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
